3197ea3b878508473db5b0b04e753410471a497d
siljalinen <linetorberg@gmail.com>
2025-06-05 12:16:27 +0200

    Update MenyVal.java

--- MenyVal.java
+++ MenyVal.java
@@ -196,4 +196,9 @@ public class MenyVal {
 
         }
     }
+
+public void avsluta() {
+        System.out.println("______________________________");
+        System.out.println("PROGRAMMET AVSLUTAS OCH SPARAS");
+    }
 }
